609744475b16c67a268fbcf8632f262872bd9403
Nitisha Khandelwal <nitisha.khandelwal@Nitishas-MBP.corp.logistimo.com>
2017-05-29 15:20:39 +0530

    updated approver json

--- src/main/java/com/logistimo/approval/models/Approver.java
+++ src/main/java/com/logistimo/approval/models/Approver.java
@@ -1,5 +1,6 @@
 package com.logistimo.approval.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 import lombok.Data;
 
@@ -12,8 +13,9 @@ public class Approver {
 
   private String userId;
 
+  @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
   private Date startTime;
 
+  @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
   private Date endTime;
-
 }
