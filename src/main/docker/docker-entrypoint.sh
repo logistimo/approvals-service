fdad5b990e7830fdcd852788ef13f9f42f630f81
Nitisha Khandelwal <nitisha@logistimo.com>
2017-06-20 18:27:09 +0530

    Docker changes

---
+++ src/main/docker/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom \
+    -Dserver.port=$SERVER_PORT \
+    -Dspring.datasource.url=$MYSQL_HOST \
+    -Dspring.datasource.username==$MYSQL_USER \
+    -Dspring.datasource.password=$MYSQL_PASS \
+    -Dconversationservice.url=$CONVERSATION_SERVICE_URL \
+    -Dspring.activemq.broker-url=$ACTIVEMQ_BROKER_URL \
+    -Dspring.activemq.user=$ACTIVEMQ_USER \
+    -Dspring.activemq.password=$ACTIVEMQ_PASSWORD \
+    -jar /approval-service.jar
+
+
