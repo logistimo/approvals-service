2a9b3f30c09dd4939c631de76ddfc201515b3055
narendiran <naren@logistimo.com>
2017-07-05 18:13:09 +0530

    Updated the dockerfiles for JMX port and header

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
+
+#
+# Copyright © 2017 Logistimo.
+#
+# This file is part of Logistimo.
+#
+# Logistimo software is a mobile & web platform for supply chain management and remote temperature monitoring in
+# low-resource settings, made available under the terms of the GNU Affero General Public License (AGPL).
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# You can be released from the requirements of the license by purchasing a commercial license. To know more about
+# the commercial license, please contact us at opensource@logistimo.com
+
 
 
 exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom \
@@ -10,6 +33,7 @@ exec java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom \
     -Dspring.activemq.broker-url=$ACTIVEMQ_BROKER_URL \
     -Dspring.activemq.user=$ACTIVEMQ_USER \
     -Dspring.activemq.password=$ACTIVEMQ_PASSWORD \
+    -javaagent:/opt/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:/opt/jmx_exporter.json \
     -jar /approval-service.jar
 
 
